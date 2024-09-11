20f1244e8ae79ed526c4c57f6fbc36c59dbe6c65
Mojtaba <mghasemi13786@gmail.com>
2024-09-11 20:03:28 +0330

    add docter view

--- doctors/views.py
+++ doctors/views.py
@@ -11,3 +11,14 @@ def create_doctor(request):
     else:
         forms = DoctorForm()
         return render(request, 'create_doctor.html', {'forms': forms})
+
+
+def add_doctor(request):
+    if request.method =='POST':
+        forms = DoctorForm(request.POST)
+        if not forms.is_valid():
+            return render(request, 'create_doctor.html', {'forms': forms})
+        forms.save()
+    else:
+        forms = DoctorForm()
+        return render(request, 'create_doctor.html', {'forms': forms})
